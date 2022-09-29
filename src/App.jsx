555b383fd0fb2bd6aa525fa144f489a5126f6637
droshev <mladen.droshev@sap.com>
2022-09-29 09:18:37 -0400

    feat: bring page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,65 @@
 import logo from './logo.svg';
-import styles from './App.module.css';
+import styles from './App.scss';
+import Shellbar from './Shellbar';
 
 import "@ui5/webcomponents/dist/Input";
 import "@ui5/webcomponents/dist/DatePicker";
 import "@ui5/webcomponents/dist/Button";
 
+import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
+import { attachThemeLoaded } from "@ui5/webcomponents-base/dist/theming/ThemeLoaded.js";
+
+
+attachThemeLoaded(async (themeName) => {
+  // create the style tag for fundamentals if missing
+  let style = document.getElementById("fd-params");
+  if (!style) {
+      style = document.createElement("style");
+      style.id = "fd-params";
+      document.head.appendChild(style);
+  }
+
+  let css = undefined;
+	switch (themeName) {
+		case "sap_horizon": css = (await import("fundamental-styles/dist/theming/sap_horizon.css")).default;
+	}
+  style.innerHTML = css;
+});
+setTheme("sap_horizon");
+
 function App() {
   let newEditTitle, newEditDue;
+
+  const shellBarTitle = "SolidJS ToDo Application";
+
 
   function addTodo() {
     alert(`Title: ${newEditTitle.value}; Due: ${newEditDue.value}`);
   }
 
   return (
-    <section>
-      <ui5-input ref={newEditTitle} placeholder="Type a task..." />
-      <ui5-date-picker ref={newEditDue} format-pattern="dd/MM/yyyy" />
-      <ui5-button on:click={addTodo} design="Emphasized">Add Todo</ui5-button>
+
+    <section class="todo-wrapper">
+      <Shellbar title={shellBarTitle} />
+      <section class="todo">
+          <div class="todo__create fd-layout-panel">
+            <div class="todo__create-wrapper fd-layout-panel__body">
+              <ui5-input id="add-input" placeholder="Type a task..." class="todo__task-name" />
+              <ui5-date-picker id="date-picker" format-pattern="dd/MM/yyyy" />
+              <ui5-button id="add-btn" on:click={addTodo} design="Emphasized">Add Todo</ui5-button>
+            </div>
+          </div>
+      </section>
+      <div class="fd-bar fd-bar--footer fd-bar--cozy">
+            <div class="fd-bar__right">
+                <div class="fd-bar__element">
+                    <button aria-label="button" class="fd-button fd-button--emphasized">Complete All</button>
+                </div>
+                <div class="fd-bar__element">
+                    <button aria-label="button" class="fd-button fd-button--transparent">Delete All</button>
+                </div>
+            </div>
+      </div>
     </section>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,11 @@
 import { render } from 'solid-js/web';
 
 import './index.css';
+import "fundamental-styles/dist/button.css";
+import "fundamental-styles/dist/bar.css";
+import "fundamental-styles/dist/layout-panel.css";
+import "fundamental-styles/dist/shellbar.css";
+import "fundamental-styles/dist/avatar.css";
 import App from './App';
 
 render(() => <App />, document.getElementById('root'));
